b42b698a3febdce20b9df063443bb6718cd8303d
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-10 00:24:30 +0300

    some problems

---
+++ Lection_01_ex7/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int xa = 40, ya = 1;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.Clear();
+int xb = 1, yb = 20;
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+        
