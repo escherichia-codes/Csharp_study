27f1bae229094aa56f302c812e4174abb3702cd1
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-10 02:34:55 +0300

    Seminar program day of week choice

---
+++ Seminar_01_ex3/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("Введите число от 1 до 7 ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else
+{
+if (x == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else
+{
+if (x == 3)
+{
+    Console.WriteLine("Среда");
+}
+else
+{
+if (x == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else
+{
+if (x == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else
+{
+if (x == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else
+{
+if (x == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Вы ввели неподходящее число!");
+}}}}}}}
