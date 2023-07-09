6e04d215b0d2aa74014875fdf50e548828897f9d
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-10 02:22:02 +0300

    Seminar program square check

---
+++ Seminar_01_ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Проверка квадратов
+
+Console.Write("Введите первое число ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x>y)
+{
+    if (y*y == x)
+    {
+        Console.Write("Да, ");
+        Console.Write(x);
+        Console.Write(" является квадратом ");
+        Console.WriteLine(y);
+    }
+    else
+    {
+        Console.WriteLine("Квадратов не найдено");
+    }
+}
+else
+{
+    if (x*x == y)
+    {
+        Console.Write("Да, ");
+        Console.Write(y);
+        Console.Write(" является квадратом ");
+        Console.WriteLine(x);
+    }
+    else
+    {
+        Console.WriteLine("Квадратов не найдено");
+    }
+}
