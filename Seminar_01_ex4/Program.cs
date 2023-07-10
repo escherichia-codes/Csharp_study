62bd2dcd888ed5a1eedc42d72e198acb6b4f305a
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-10 03:05:02 +0300

    Seminar program from -n to n

---
+++ Seminar_01_ex4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите целое число больше нуля: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -n;
+while (count <= n)
+{
+    Console.Write($"{count} ");
+    count++;
+}
