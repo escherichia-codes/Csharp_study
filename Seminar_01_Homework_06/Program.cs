6153c3e46c937c67c93c20717b2d4fe8b41832ab
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-10 03:38:19 +0300

    Homework odd numbers

---
+++ Seminar_01_Homework_06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write("Это число четное");
+}
+else
+{
+    Console.Write("Это число НЕчетное");
+}
