ba668c322f7b0304ae2aa285440e1f1a8b030f9b
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-10 03:33:49 +0300

    Homework max of three

---
+++ Seminar_01_Homework_04/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+Console.Write($"Максимальное число из трех: {max}");
