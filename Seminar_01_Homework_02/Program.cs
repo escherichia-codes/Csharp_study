a41d8673ac73b45c8092fa2a246dba343434cd91
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-10 03:25:23 +0300

    Homework max of two

---
+++ Seminar_01_Homework_02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.Write($"Максимальное число: {a}");
+}
+else
+{
+    Console.Write($"Максимальное число: {b}");
+}
