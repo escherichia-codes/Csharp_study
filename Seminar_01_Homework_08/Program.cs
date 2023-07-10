2c9d9a01951a7539ab968763908196876afd486b
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-10 03:49:43 +0300

    Homework even numbers from one to n

---
+++ Seminar_01_Homework_08/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число больше нуля: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if (n <= 0)
+{
+    Console.Write("Некорректное число");
+}
+else
+{
+    while (i <= n)
+    {Console.Write($"{i} ");
+    i += 2;}
+}
