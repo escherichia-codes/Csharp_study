30dd385e6e9c00d1ec82ebc336cee4189da4c957
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-13 16:20:43 +0300

    Maximun of array

---
+++ Lection_02_ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int[] Array)
+{
+    int result = Array[0];
+    int index = 1;
+    while (index < Array.Length)
+    {
+        if (Array[index] > result) result = Array[index];
+        index = index + 1;
+    }
+    return result;
+}
+
+int[] array2 = {78, 66, 12, 78, 66, 12, 78, 6600, 12};
+
+int maxfin = Max(array2);
+
+Console.WriteLine(maxfin);
