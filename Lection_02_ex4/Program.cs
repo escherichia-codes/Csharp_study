143b5277d5a41e97b0c0a24da5d8e5c15fbdb1bc
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-14 02:03:11 +0300

    Arrays

---
+++ Lection_02_ex4/Program.cs
@@ -0,0 +1,19 @@
+﻿// поиск индекса элемента с заданным значением
+
+int[] array = { 1, 100, 3, 67, 45, 67, 78 };
+
+int index = 0;
+int find = 67;
+
+
+while (index < array.Length)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
---
+++ Lection_02_ex5/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(int[] array1)
+{
+    int i = 0;
+    while (i<array1.Length)
+    {
+        array1[i] = new Random().Next(1, 100);
+        i++;
+    }
+}
+
+void PrintArray(int[] array2)
+{
+    int j = 0;
+    while (j<array2.Length)
+    {
+        Console.Write($"{array2[j]} ");
+        j++;
+    }
+}
+
+int[] massive = new int[10];
+FillArray(massive);
+PrintArray(massive);
+
