88ed4c05b776ce3eccb6046d949523422fdc0d45
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-13 15:54:16 +0300

    Lection max of 9 function example

---
+++ Lection_02_ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 78;
+int a2 = 66;
+int a3 = 12;
+int b1 = 78;
+int b2 = 66;
+int b3 = 12;
+int c1 = 78;
+int c2 = 6600;
+int c3 = 12;
+
+// int max1 = Max(a1, a2, a3);
+// int max2 = Max(b1, b2, b3);
+// int max3 = Max(c1, c2, c3);
+// int maxfin = Max(max1, max2, max3);
+
+int maxfin = Max(Max(a1, a2, a3), Max(b1, b2, b3), Max(c1, c2, c3));
+
+Console.WriteLine(maxfin);
