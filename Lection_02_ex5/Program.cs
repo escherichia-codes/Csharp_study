1bb65c462dfd74c174bb7c2faa5df160aadc19ce
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-14 02:59:32 +0300

    Arrays and functions

--- Lection_02_ex5/Program.cs
+++ Lection_02_ex5/Program.cs
@@ -18,7 +18,26 @@ void PrintArray(int[] array2)
     }
 }
 
+int ElementIndex(int[] array3, int find)
+{
+    int k = 0;
+    int element = -1;
+    while (k<array3.Length)
+    {
+        if (array3[k] == find)
+        {
+            element = k;
+            break;
+        }
+        k++;
+    }
+    return element;
+}
+
 int[] massive = new int[10];
 FillArray(massive);
 PrintArray(massive);
+Console.WriteLine();
+int pos = ElementIndex(massive, 4);
+Console.WriteLine(pos);
 
