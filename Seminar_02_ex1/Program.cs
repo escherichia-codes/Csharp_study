53cf2894dbc86324dc5c4c37857c67ae7acb5ce7
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-14 04:15:14 +0300

    Join array

---
+++ Seminar_02_ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] nArray = new int[n];
+for (int i = 0; i < nArray.Length; i++)
+{
+    nArray[i] = new Random().Next(-10, 11);
+}
+// for (int i = 0; i < nArray.Length; i++)
+// {
+//     Console.Write($"{nArray[i]} ");
+// }
+Console.Write($"[{string.Join(", ", nArray)}]");
