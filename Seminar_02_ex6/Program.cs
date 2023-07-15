610052ed74ade17f8ce37bce02bb78a10bdfdfc1
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-15 23:44:35 +0300

    Examples with divisibility

---
+++ Seminar_02_ex6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A%B == 0)
+{
+    Console.Write("Первое число кратно второму");
+}
+else
+{
+    Console.Write($"Первое число не кратно второму, остаток {A%B}");
+}
