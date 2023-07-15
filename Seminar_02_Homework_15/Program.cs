9691f73e6962ebdc92c5a97372b0d58c9945690d
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-16 00:19:20 +0300

    Homework days of week

---
+++ Seminar_02_Homework_15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите номер дня недели (от 1 до 7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+
+if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5)
+{
+    Console.Write("Это будний день");
+}
+else
+{
+    if (day == 6 || day == 7)
+    {
+        Console.Write("Ура, это выходной");
+    }
+    else
+    {
+        Console.Write("Кажется, вы ввели что-то не то");
+    }
+}
