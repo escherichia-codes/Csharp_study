5e7260c2535fb2a9e883ff5a437d89732740d71a
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-15 23:55:51 +0300

    Examples with divisibility and AND

---
+++ Seminar_02_ex7/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число (делимое): ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число (первый делитель): ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число (второй делитель): ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if (A%B == 0 && A%C == 0)
+{
+    Console.Write("Первое число кратно второму и третьему одновременно");
+}
+else
+{
+    Console.Write("Первое число НЕ кратно второму и третьему одновременно");
+}
