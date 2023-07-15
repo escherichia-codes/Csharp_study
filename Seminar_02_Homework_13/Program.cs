2785fda5565f649c4a4118484d5e1415aac29b6e
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-16 00:51:07 +0300

    Homework third number

---
+++ Seminar_02_Homework_13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое число больше нуля: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 100)
+{
+    Console.WriteLine("Это число меньше 100, в нем нет третьей цифры");
+}
+else
+{
+    while (a > 999)
+    {
+        a = a/10;
+    }
+        
+    int a3 = a%10;
+    Console.Write($"Третья цифра это: {a3}");
+}
+
+
