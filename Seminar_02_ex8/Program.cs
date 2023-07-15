f7f8509d7d3e5c10ffbf42e08d73fa16cb3c1724
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-16 00:50:08 +0300

    Examples with squares

---
+++ Seminar_02_ex8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+
+if (A*A == B || B*B == A)
+{
+    if (A*A == B) 
+    {Console.WriteLine($"Число {B} - квадрат числа {A}");}
+    else 
+    {Console.WriteLine($"Число {A} - квадрат числа {B}");}
+}
+else
+{
+    Console.Write("Ни одно число не является квадратом второго");
+}
