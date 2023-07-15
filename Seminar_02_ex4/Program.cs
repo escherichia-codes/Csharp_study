1009c1acd16a341b11fceba92e4bdf61bf849af4
Ekaterina <e.s.cherepanova@gmail.com>
2023-07-15 23:16:16 +0300

    Examples with division by 10 and 100

---
+++ Seminar_02_ex4/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = new Random().Next(10,100);
+Console.Write($"Случайное двузначное число: {a}, его наибольшая цифра это ");
+int a1 = a/10;
+int a2 = a%10;
+if (a1>a2)
+{
+    Console.WriteLine(a1);
+}
+else
+{
+    Console.WriteLine(a2);
+}
