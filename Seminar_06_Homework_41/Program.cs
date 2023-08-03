7a9296fdf2b181c9c11b0d29af77900879ac667f
Ekaterina <e.s.cherepanova@gmail.com>
2023-08-04 01:47:21 +0300

    Seminar 06 Homework

---
+++ Seminar_06_Homework_41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int CountPlus(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+
+
+Console.Write("Введите целые числа через пробел: ");
+int[] array1 = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+Console.WriteLine($"[{String.Join(", ", array1)}]");
+
+Console.Write($"Число чисел больше 0: {CountPlus(array1)}");
---
+++ Seminar_06_Homework_43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double[] Dot (double a1, double c1, double a2, double c2)
+{
+    double x = (c2 - c1)/(a1 - a2);
+    double y = a1*x + c1;
+    double[] arr = {x, y};
+    return arr;
+}
+
+Console.WriteLine("Для формирования системы уравнений вида: y = k1 * x + b1, y = k2 * x + b2");
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"[{String.Join(", ", Dot (k1, b1, k2, b2))}]");
+
+
